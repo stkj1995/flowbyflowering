459c05efd4aec9981469f1d1ccef9fa245a9aa3e
Sophie <soph1155@stud.kea.dk>
2024-12-14 12:01:52 +0100

    tilføj fontWeight

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -91,14 +91,11 @@ export default {
         },
   
         /* Fontvægt */
-        // fontWeight: {
-        //   regular: "400",
-        //   medium: "500",
-        //   semibold: "600",
-        //   bold: "700",
-        //   extrabold: "800",
-        //   black: "900",
-        // },
+        fontWeight: {
+          regular: "400",
+          medium: "500",
+          bold: "700",
+        },
   
         /* Border størrelser */
         // borderWidth: {
