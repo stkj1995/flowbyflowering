a659943795cebb23a7fcb92df4f745146c87b963
Sophie <soph1155@stud.kea.dk>
2024-12-19 13:23:51 +0100

    udkommenter tailwindconfigjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+// tailwind.config.js (Udkommenteret version)
+module.exports = {
+  // content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       transparent: "transparent",
+  //       current: "currentColor",
+  //       // Flere farver her...
+  //     },
+  //   },
+  // },
+  // plugins: [
+  //   require("@tailwindcss/typography"),
+  //   require("@tailwindcss/forms"),
+  //   require("@tailwindcss/aspect-ratio"),
+  //   require("@tailwindcss/container-queries"),
+  // ],
 }
-
